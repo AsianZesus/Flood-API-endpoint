b50eeaf85d9c8aeefe5e4124e7fecab62be27185
AsianZesus <109887524+AsianZesus@users.noreply.github.com>
2023-04-13 10:01:30 +0545

    Register endpoint

--- index.js
+++ index.js
@@ -70,6 +70,33 @@ app.post('/login', async (req, res) => {
   }
 });
 
+// endpoint for user registration
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+
+    // query the database to see if a user with the given email already exists
+    const { rows } = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    if (rows.length > 0) {
+      // if a user with the given email already exists, return an error response
+      return res.status(409).json({ error: 'User already exists.' });
+    }
+
+    // insert the new user into the database
+    const query = 'INSERT INTO users (email, password) VALUES ($1, $2)';
+    await pool.query(query, [email, password]);
+
+    // return a success response
+    return res.status(200).json({ message: 'User registered successfully.' });
+
+  } catch (error) {
+    // log the error and return an error response
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+
 // handle 404 errors
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Endpoint not found' });
