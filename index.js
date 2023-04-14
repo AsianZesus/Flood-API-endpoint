93069910e4160f4a04f4374cd0c38930dbf3838d
AsianZesus <109887524+AsianZesus@users.noreply.github.com>
2023-04-14 14:27:13 +0545

    JWT token testing

--- index.js
+++ index.js
@@ -62,14 +62,14 @@ function authenticate(req, res, next) {
 // API endpoint for registering a new user
 app.post('/register', async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const {email, password } = req.body;
     // hash the password before storing it in the database
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // insert the new user into the "users" table
     const result = await pool.query(
-      'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email',
-      [name, email, hashedPassword]
+      'INSERT INTO users (email, password) VALUES ($1, $2, $3) RETURNING id, email',
+      [email, hashedPassword]
     );
 
     // create a JWT for the new user
